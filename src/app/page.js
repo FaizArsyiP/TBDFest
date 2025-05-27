fb0f39e6a042cf6bea4d1bdc95604e986959eb22
Dapreall <dhafarelhariyanto2005@mail.ugm.ac.id>
2025-05-27 12:57:37 +0700

    unch lagi

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,17 @@
 import Image from "next/image";
 
 export default function Home() {
+  const fontFamily = "'DM Serif Display', serif";
+
   return (
     <div>
-      <div className="text-2xl font-bold text-center mt-10">
-      hello world
-    </div>
+      <div className="text-2xl font-bold text-center mt-10" style={{ fontFamily }}>
+        hello world
+      </div>
 
-    <button className="bg-blue-500 text-white px-4 py-2 rounded mt-5 hover:" href="/about">
-      Click Me
-    </button>
-    
+      <button className="bg-blue-500 text-white px-4 py-2 rounded mt-5 hover:" href="/about" >
+        Click Me
+      </button>
     </div>
   );
 }
